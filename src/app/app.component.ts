133ad58283175b225e41e558c747171127392c05
modafa-pc <sakpakd@gmail.com>
2024-03-18 14:23:00 +0100

    add home components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.png" alt ="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+  ],
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-course';
+  title = 'homes';
 }
